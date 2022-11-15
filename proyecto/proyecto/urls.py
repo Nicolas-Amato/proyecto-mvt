abda8e902d7ef7c34e4fd2df3fc0e0d914858329
Nicolas_Amato <nicolasamato@outlook.com>
2022-11-15 12:49:40 -0300

    CAMBIOS TRATANDO DE SOLUCIONAR

--- proyecto/proyecto/urls.py
+++ proyecto/proyecto/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path('', include('MiApp.urls'))
+    re_path('', include('MiApp.urls') )
 ]
 
