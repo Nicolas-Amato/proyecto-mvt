48f693a984ae81fdafb84a598d1de8cc92875dfa
Nicolas_Amato <nicolasamato@outlook.com>
2022-11-15 12:30:02 -0300

    cambio

--- proyecto/MiApp/views.py
+++ proyecto/MiApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import club_deportivo, profesor, alumno
-from .forms import club_deportivoForm, profesorForm, alumnoForm
+from models import club_deportivo, profesor, alumno
+from forms import club_deportivoForm, profesorForm, alumnoForm
 
 
 def mostrar_index(request):
